818f50f8f7031a79c0f4690dc2f589843469404a
Ali Muhammad <alimuhammadfc5@gmail.com>
2022-11-06 16:50:25 +0500

    update readme

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
-type Data = {
-  name: string
-}
+export type HelloData = {
+	greetings?: string;
+	error?: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<HelloData>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+	// Create a new ratelimiter, that allows 10 requests per 10 seconds
+	const ratelimit = new Ratelimit({
+		redis: Redis.fromEnv(),
+		limiter: Ratelimit.slidingWindow(5, "10 s"),
+	});
+	console.log("Redis is running");
+
+	// Use a constant string to limit all requests with a single ratelimit
+	// Or use a userID, apiKey or ip address for individual limits.
+	const identifier = "api";
+	const { success } = await ratelimit.limit(identifier);
+
+	if (!success) {
+		console.log("Rate limit");
+		res.status(406).json({ error: "Too many requests" });
+	}
+
+	res.status(200).json({ greetings: "Hello from api" });
 }
